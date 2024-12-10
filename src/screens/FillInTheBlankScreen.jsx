25508685dd316937e50983c41313828f1fbf1de9
MercyBsoft <mercy@dbcyelagiri.edu.in>
2024-12-10 18:02:17 +0530

    fill in the blank screen UI enhancement

--- src/screens/FillInTheBlankScreen.jsx
+++ src/screens/FillInTheBlankScreen.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native';
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import FastImage from 'react-native-fast-image';
-import { Dropdown } from 'react-native-element-dropdown';
 import { StyleSheet } from 'react-native';
 import { useState } from 'react';
 import { TextInput } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 function FillInTheBlankScreen({ navigation }) {
-  const [value, setValue] = useState(null);
 
   const [place, setPlace] = useState('');
   const [adjective, setAdjective] = useState('');
@@ -18,35 +16,30 @@ function FillInTheBlankScreen({ navigation }) {
   const [verb, setVerb] = useState('');
   const [thing, setThing] = useState('');
   const [weatherAdjective, setWeatherAdjective] = useState('');
-
-  const data = [
-    { label: 'Apple', value: 'apple' },
-    { label: 'Banana', value: 'banana' },
-    { label: 'Orange', value: 'orange' },
-  ];
+  const insets= useSafeAreaInsets();
   return (
-    <View style={styles.container}>
+    <View style={[styles.container,{ paddingTop: insets.top}]}>
+      <ImageBackground source={require('../assets/images/bg1.png')} style={styles.backgroundImage}>
       {/* Header Section */}
       <View style={styles.header}>
         <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
           <View style={styles.circle}>
-            <Ionicons name="arrow-back" size={24} color="#333" />
+            <Ionicons name="arrow-back" size={24} color="white" />
           </View>
         </TouchableOpacity>
         <Text style={styles.headerTitle}> Fill in the Blank </Text>
+        <Ionicons name="arrow-back" size={24} color="white" style={styles.bulbIcon} />
       </View>
 
       {/* Center Content Section */}
       <View style={styles.content}>
         <View style={styles.sentenceRow}>
           {/* Buttons */}
-          <Text style={styles.sentenceText}>
-            On a sunny afternoon, I walked to the
-          </Text>
+          <Text style={styles.sentenceText}>On a sunny afternoon, I walked to the</Text>
           <TextInput
             style={styles.input}
             placeholder="place"
-            placeholderTextColor={'#ccc'}
+            placeholderTextColor={'#fdd0a7'}
             value={place}
             onChangeText={setPlace}
           />
@@ -54,23 +47,18 @@ function FillInTheBlankScreen({ navigation }) {
           <TextInput
             style={styles.input}
             placeholder="adjective"
-            placeholderTextColor={'#ccc'}
+            placeholderTextColor={'#fdd0a7'}
             value={adjective}
             onChangeText={setAdjective}
           />
           <Text style={styles.sentenceText}> </Text>
-          <TextInput
-            style={styles.input}
-            placeholder="friend or pet"
-            placeholderTextColor={'#ccc'}
-            value={friendOrPet}
-            onChangeText={setFriendOrPet}
+          <TextInput style={styles.input} placeholder="friend or pet" placeholderTextColor={'#fdd0a7'} value={friendOrPet} onChangeText={setFriendOrPet}
           />
           <Text style={styles.sentenceText}> , carrying a </Text>
           <TextInput
             style={styles.input}
             placeholder="noun"
-            placeholderTextColor={'#ccc'}
+            placeholderTextColor={'#fdd0a7'}
             value={noun}
             onChangeText={setNoun}
           />
@@ -78,7 +66,7 @@ function FillInTheBlankScreen({ navigation }) {
           <TextInput
             style={styles.input}
             placeholder="verb"
-            placeholderTextColor={'#ccc'}
+            placeholderTextColor={'#fdd0a7'}
             value={verb}
             onChangeText={setVerb}
           />
@@ -86,7 +74,7 @@ function FillInTheBlankScreen({ navigation }) {
           <TextInput
             style={styles.input}
             placeholder="thing"
-            placeholderTextColor={'#ccc'}
+            placeholderTextColor={'#fdd0a7'}
             value={thing}
             onChangeText={setThing}
           />
@@ -94,27 +82,14 @@ function FillInTheBlankScreen({ navigation }) {
           <TextInput
             style={styles.input}
             placeholder="adjective"
-            placeholderTextColor={'#ccc'}
+            placeholderTextColor={'#fdd0a7'}
             value={weatherAdjective}
             onChangeText={setWeatherAdjective}
           />
           <Text style={styles.sentenceText}> weather. </Text>
         </View>
-
-        {/* On a sunny afternoon, I walked to the ____________(palce) with 
-              my _________[adjective] ___________[friend or pet], carrying a ________[noun], and we decided to _______[verb]
-              near the _________[thing], enjoying the [adjective] weather.  */}
       </View>
-
-      {/* Advertisement Section */}
-      <View style={styles.adContainer}>
-        <Text style={styles.adText}>Advertisement</Text>
-        <FastImage
-          source={{ uri: 'https://via.placeholder.com/300x100' }} // Replace with your ad image or SDK
-          style={styles.adImage}
-          resizeMode="contain"
-        />
-      </View>
+      </ImageBackground>
     </View>
   )
 }
@@ -125,17 +100,24 @@ export default FillInTheBlankScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffeedd',
+    backgroundColor: '#fff',
   },
-
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  bulbIcon: {
+    marginLeft: 10,
+    opacity:0
+  },
   // Header styles
   header: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor: '#f0f0f0',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
     paddingHorizontal: 15,
-    gap: 100,
     paddingVertical: 10,
     borderBottomWidth: 1,
     borderColor: '#ddd',
@@ -145,89 +127,58 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   circle: {
-    width: 40,
-    height: 40,
+    width: 35,
+    height: 35,
     borderRadius: 20,
-    backgroundColor: '#ddd',
+    backgroundColor: '#f18927',
     justifyContent: 'center',
     alignItems: 'center',
   },
 
   headerTitle: {
-    fontSize: 18,
+    color: '#04acb8',
     fontWeight: 'bold',
-    color: '#333',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textShadowColor: '#d8d6d6',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 1,
+    letterSpacing: 1,
   },
 
   // Content styles
   content: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     paddingHorizontal: 20,
-    paddingVertical: 50,
   },
   sentenceRow: {
     flexWrap: 'wrap',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    width: '90%',
+    justifyContent: 'space-around',
+    width: '100%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 15,
   },
   sentenceText: {
-    fontSize: 16,
+    fontSize: 18,
     fontWeight: 'bold',
-    color: '#333',
+    color: '#04acb8',
     marginRight: 5,
   },
   input: {
       borderBottomWidth: 1,
-    borderColor: '#bbb',
+    borderColor: '#f18927',
     marginHorizontal: 5,
     paddingHorizontal: 5,
     fontSize: 16,
-    color: '#333',
+    color: '#f18927',
     minWidth: 10,
     maxWidth: 120,
     textAlign: 'left',
-  },
-  dropdown: {
-    width: 150,
-    height: 40,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    paddingHorizontal: 10,
-    justifyContent: 'center',
-  },
-  placeholder: {
-    fontSize: 14,
-    color: '#aaa',
-  },
-  selectedText: {
-    fontSize: 14,
-    color: '#333',
-  },
-
-  // Advertisement section styles
-  adContainer: {
-    width: '100%',
-    paddingVertical: 10,
-    backgroundColor: '#f2f2f2',
-    borderTopWidth: 1,
-    borderColor: '#ddd',
-    alignItems: 'center',
-  },
-  adText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#ff0000',
-    marginBottom: 5,
-  },
-  adImage: {
-    width: 300,
-    height: 60,
-  },
+  }
 });
 
