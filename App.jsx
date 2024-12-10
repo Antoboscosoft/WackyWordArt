cbe0a9ea78a9d7fb6cddd5313f511e4f711a4cab
ravishnu17 <ravinshnu@dbcyelagiri.edu.in>
2024-12-10 13:41:15 +0530

    Splash screen

--- App.jsx
+++ App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MainNavigator from './src/navigations/MainNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App() {
+  
   return (
     <GestureHandlerRootView>
-      <SafeAreaProvider>
+      <SafeAreaProvider >
         <MainNavigator />
       </SafeAreaProvider>
     </GestureHandlerRootView>
--- src/navigations/MainNavigator.jsx
+++ src/navigations/MainNavigator.jsx
@@ -12,28 +12,55 @@ import WackyWordWheelScreen from '../screens/WackyWordWheelScreen';
 import FillInTheBlankScreen from '../screens/FillInTheBlankScreen';
 import MakeYourownScreen from '../screens/MakeYourownScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { StatusBar, View } from 'react-native';
+import FastImage from 'react-native-fast-image';
+
+
+const Splash = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <FastImage source={require('../assets/images/splash.png')} resizeMode='cover' style={{ width: '100%', height: '100%' }} />
+    </View>
+  );
+}
 
 const Stack = createNativeStackNavigator();
 
 function MainNavigator() {
+ 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    StatusBar.setHidden(true);
+    StatusBar.setTranslucent(true);
+
+    
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Home' screenOptions={{
-            headerShown: false,
-            animationEnabled: false,
-            }}>
-            <Stack.Screen name='Home' component={HomeScreen} />
-            <Stack.Screen name='Game' component={GameScreen} />
-            <Stack.Screen name='HowToPlayScreen' component={HowToPlayScreen} />
-            <Stack.Screen name='Profile' component={ProfileScreen} />
-            <Stack.Screen name='Progress' component={PlayScreen} />
-            <Stack.Screen name="Settings" component={SettingsScreen} />
-            <Stack.Screen name="TutorialScreen" component={TutorialScreen} />
-            <Stack.Screen name='LetsLearnScreen' component={LetsLearnScreen} />
-            <Stack.Screen name='WackWordArtScreen' component={WackyWordWheelScreen} />
-            <Stack.Screen name='FillInTheBlankScreen' component={FillInTheBlankScreen} />
-            <Stack.Screen name='MakeYourOwnScreen' component={MakeYourownScreen} />
-        </Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{
+        headerShown: false,
+        animation: 'fade',
+        navigationBarHidden:true
+      }}>
+        <Stack.Screen name='Home' component={isLoading ? Splash : HomeScreen} />
+        <Stack.Screen name='Game' component={GameScreen} />
+        <Stack.Screen name='HowToPlayScreen' component={HowToPlayScreen} />
+        <Stack.Screen name='Profile' component={ProfileScreen} />
+        <Stack.Screen name='Progress' component={PlayScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="TutorialScreen" component={TutorialScreen} />
+        <Stack.Screen name='LetsLearnScreen' component={LetsLearnScreen} />
+        <Stack.Screen name='WackWordArtScreen' component={WackyWordWheelScreen} />
+        <Stack.Screen name='FillInTheBlankScreen' component={FillInTheBlankScreen} />
+        <Stack.Screen name='MakeYourOwnScreen' component={MakeYourownScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
