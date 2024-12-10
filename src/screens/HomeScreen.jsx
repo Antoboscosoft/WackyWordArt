63220475b79f38908c9735d16565a87f140bd1f4
MercyBsoft <mercy@dbcyelagiri.edu.in>
2024-12-10 14:09:25 +0530

    home page UI enhancement

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,269 +1,303 @@
-import React, { useRef, useState } from 'react'
-import { FlatList, Modal, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import React, {useRef} from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import FastImage from 'react-native-fast-image';
-import { DrawerLayout } from 'react-native-gesture-handler';
-// import { Text, View } from 'react-native-reanimated/lib/typescript/Animated'
-import PageIcon from 'react-native-vector-icons/SimpleLineIcons';
+import {DrawerLayout} from 'react-native-gesture-handler';
+import MatirialIcon from 'react-native-vector-icons/MaterialIcons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // For drawer items
-import BulbIcon from 'react-native-vector-icons/MaterialIcons';
+import PlayIcon from 'react-native-vector-icons/AntDesign';
+import MenuIcon from 'react-native-vector-icons/Entypo';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+function HomeScreen({navigation}) {
+  const drawerRef = useRef(null);
+  const insets= useSafeAreaInsets();
+  // Drawer Content with Icons
+  const renderDrawerContent = () => (
+    // return (
+    <View style={styles.drawerContainer}>
+      <Text
+        style={styles.drawerTitle}
+        onPress={() => drawerRef.current.closeDrawer()}
+        hitslop={20}>
+        <Text style={[{alignSelf: 'flex-start', marginRight: 10}]}>
+          {' '}
+          ☰ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </Text>
+        Menu
+      </Text>
+      <TouchableOpacity
+        style={styles.drawerItem}
+        onPress={() => {
+          drawerRef.current.closeDrawer();
+          navigation.navigate('Profile');
+        }}>
+        <Icon
+          name="account"
+          size={24}
+          color="#3b5a9a"
+          style={styles.drawerItemIcon}
+        />
+        <Text style={styles.drawerItemText}>Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.drawerItem}
+        onPress={() => {
+          drawerRef.current.closeDrawer();
+          navigation.navigate('Game');
+        }}>
+        <Icon
+          name="gamepad-variant"
+          size={24}
+          color="#3b5a9a"
+          style={styles.drawerItemIcon}
+        />
+        <Text style={styles.drawerItemText}>Games</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.drawerItem}
+        onPress={() => {
+          drawerRef.current.closeDrawer();
+          navigation.navigate('TutorialScreen');
+        }}>
+        <Icon
+          name="book-open-variant"
+          size={24}
+          color="#3b5a9a"
+          style={styles.drawerItemIcon}
+        />
+        <Text style={styles.drawerItemText}>Tutorial Screen</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.drawerItem}
+        onPress={() => {
+          drawerRef.current.closeDrawer();
+          navigation.navigate('Progress');
+        }}>
+        <Icon
+          name="chart-bar"
+          size={24}
+          color="#3b5a9a"
+          style={styles.drawerItemIcon}
+        />
+        <Text style={styles.drawerItemText}>Progress</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.drawerItem}
+        onPress={() => {
+          drawerRef.current.closeDrawer();
+          navigation.navigate('Settings');
+        }}>
+        <Icon
+          name="cog"
+          size={24}
+          color="#3b5a9a"
+          style={styles.drawerItemIcon}
+        />
+        <Text style={styles.drawerItemText}>Settings</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.drawerItem}
+        onPress={() => {
+          drawerRef.current.closeDrawer();
+          navigation.navigate('Home');
+        }}>
+        <Icon
+          name="logout"
+          size={24}
+          color="#3b5a9a"
+          style={styles.drawerItemIcon}
+        />
+        <Text style={styles.drawerItemText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+    // );
+  );
+  // }
 
-function HomeScreen({ navigation }) {
-    const drawerRef = useRef(null);
-
-    // Drawer Content with Icons
-    const renderDrawerContent = () => (
-        // return (
-        <View style={styles.drawerContainer}>
-            <Text style={styles.drawerTitle} onPress={() => drawerRef.current.closeDrawer()} hitslop={20}>
-                <Text style={[{ alignSelf: 'flex-start', marginRight: 10 }]}> ☰    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Text>
-                Menu
-            </Text>
-            <TouchableOpacity
-                style={styles.drawerItem}
-                onPress={() => {
-                    drawerRef.current.closeDrawer();
-                    navigation.navigate('Profile');
-                }}>
-                <Icon name="account" size={24} color="#3b5a9a" style={styles.drawerItemIcon} />
-                <Text style={styles.drawerItemText}>Profile</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.drawerItem}
-                onPress={() => {
-                    drawerRef.current.closeDrawer();
-                    navigation.navigate('Game');
-                }}>
-                <Icon name="gamepad-variant" size={24} color="#3b5a9a" style={styles.drawerItemIcon} />
-                <Text style={styles.drawerItemText}>Games</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.drawerItem}
-                onPress={() => {
-                    drawerRef.current.closeDrawer();
-                    navigation.navigate('TutorialScreen');
-                }}>
-                <Icon name="book-open-variant" size={24} color="#3b5a9a" style={styles.drawerItemIcon} />
-                <Text style={styles.drawerItemText}>Tutorial Screen</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.drawerItem}
-                onPress={() => {
-                    drawerRef.current.closeDrawer();
-                    navigation.navigate('Progress');
-                }}>
-                <Icon name="chart-bar" size={24} color="#3b5a9a" style={styles.drawerItemIcon} />
-                <Text style={styles.drawerItemText}>Progress</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.drawerItem}
-                onPress={() => {
-                    drawerRef.current.closeDrawer();
-                    navigation.navigate('Settings');
-                }}>
-                <Icon name="cog" size={24} color="#3b5a9a" style={styles.drawerItemIcon} />
-                <Text style={styles.drawerItemText}>Settings</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.drawerItem}
-                onPress={() => {
-                    drawerRef.current.closeDrawer();
-                    navigation.navigate('Home');
-                }}>
-                <Icon name="logout" size={24} color="#3b5a9a" style={styles.drawerItemIcon} />
-                <Text style={styles.drawerItemText}>Logout</Text>
-            </TouchableOpacity>
+  return (
+    <DrawerLayout
+      ref={drawerRef}
+      drawerWidth={250}
+      drawerPosition="left"
+      renderNavigationView={renderDrawerContent}
+      drawerType="slide">
+      <View style={[styles.container,{ paddingTop: insets.top}]}>
+        {/* Top bar */}
+        <View style={[styles.topBar, {paddingTop: 0}]}>
+          <TouchableOpacity onPress={() => drawerRef.current.openDrawer()}>
+            <MenuIcon name="menu" style={styles.menu}/>
+          </TouchableOpacity>
+          <Text style={styles.title}>Home</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
+            <MatirialIcon name="settings" style={styles.menu} />
+          </TouchableOpacity>
         </View>
-        // );
-    );
-    // }
 
-    return (
-        <DrawerLayout
-            ref={drawerRef}
-            drawerWidth={250}
-            drawerPosition="left"
-            renderNavigationView={renderDrawerContent}
-            drawerType='slide'>
-            <View style={styles.container}>
-                {/* Top bar */}
-                <View style={styles.topBar}>
-                    {/* Menu Icon */}
-                    <TouchableOpacity onPress={() => drawerRef.current.openDrawer()}>
-                        {/* <Text style={styles.homeheadicon}>☰</Text> */}
-                        <PageIcon name="menu" size={28} color="#3b5a9a" />
-                    </TouchableOpacity>
-                    {/* Menu */}
-                    <Text style={styles.title}>Home</Text>
-                    {/* Settings Icon */}
-                    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-                        {/* <Text style={styles.homeheadicon}>⚙️</Text> */}
-                        <PageIcon name="settings" size={28} color="#3b5a9a" />
-                    </TouchableOpacity>
-                </View>
-
-                {/* Main section */}
-                <View style={styles.content}>
-                    {/* <Text style={[styles.welcomeText, { color: '#3b5a9a', position: 'absolute', top: 200 }]}>
-                        Welcome to WWF Kids!
-                    </Text> */}
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => navigation.navigate('HowToPlayScreen')}>
-                <BulbIcon name="emoji-objects" size={35} color="#FF69B4" style={styles.bulbIcon} />
-
-                        <Text style={styles.buttonText}> How to Play </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        style={[styles.button, { backgroundColor: '#146edd' }]}
-                        onPress={() => navigation.navigate('Progress')}>
-                        <Text style={styles.buttonText}> Play   ▶   </Text>
-                    </TouchableOpacity>
-                    <FastImage
-                        style={[styles.zebraImage]}
-                        source={require('../assets/images/zebraY.png')}
-                    />
-                </View>
-
-                {/* Bottom section */}
-                <View style={styles.adContainer}>
-                    <Text style={styles.adText}>Advertisement</Text>
-                    {/* Placeholder for an ad image or component */}
-                    <FastImage
-                        style={styles.adImage}
-                        source={{ uri: 'https://via.placeholder.com/300x100' }} // Replace with your ad image or SDK component
-                        resizeMode="contain"
-                    />
-                </View>
+        {/* Main section */}
+        <View>
+          <ImageBackground
+            source={require('../assets/images/homebg.jpeg')}
+            style={styles.backgroundImage}>
+            <View style={styles.button}>
+              <TouchableOpacity style={styles.combineButton}
+                onPress={() => navigation.navigate('HowToPlayScreen')}>
+                <PlayIcon name="play" size={35} color="white" />
+                <Text style={styles.buttonText}> How to Play </Text>
+              </TouchableOpacity>
             </View>
-        </DrawerLayout>
-    );
+            <View style={styles.button}>
+              <TouchableOpacity style={styles.combineButton1}
+                onPress={() => navigation.navigate('HowToPlayScreen')}>
+                <PlayIcon name="play" size={35} color="white" />
+                <Text style={styles.buttonText}> Play </Text>
+              </TouchableOpacity>
+            </View>
+          </ImageBackground>
+        </View>
+        {/* Bottom section */}
+        <View style={styles.adContainer}>
+          <Text style={styles.adText}>Advertisement</Text>
+          {/* Placeholder for an ad image or component */}
+          <FastImage
+            style={styles.adImage}
+            source={{uri: 'https://via.placeholder.com/300x100'}} // Replace with your ad image or SDK component
+            resizeMode="contain"
+          />
+        </View>
+      </View>
+    </DrawerLayout>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#ffeedd', //'#fff',
-        // alignItems: 'center',
-    },
-    topBar: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 15,
-        backgroundColor: '#dfd5cb', //'#fff',
-    },
-    homeheadicon: {
-        fontSize: 30,
-        color: '#3b5a9a',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#3b5a9a', //'#3b9a67',
-    },
-    content: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    welcomeText: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#3b5a9a',
-        marginBottom: 20,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  topBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: '#fff',
+  },
+  menu:{
+    color: '#f18927',
+    fontSize: 32,
+  },
+  homeheadicon: {
+    fontSize: 30,
+    color: '#3b5a9a',
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#3b5a9a',
+    marginBottom: 20,
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  combineButton:{
+    marginTop: 200,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f18927',
+    borderRadius: 30,
+    padding: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'cursive',
+    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 5,
+  },
+  title: {
+    color: '#04acb8',
+    fontWeight: 'bold',
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'cursive',
+    textShadowColor: '#d8d6d6',
+    textShadowOffset: {width: 2, height: 2},
+    textShadowRadius: 1,
+    letterSpacing: 1,
+  },
+  combineButton1:{
+    marginTop: 450,
+    flexDirection: 'row',
+    justifyContent: 'end',
+    alignItems: 'center',
+    backgroundColor: '#04acb8',
+    borderRadius: 30,
+    padding: 15,
+  },
+  // Drawer styles
+  drawerContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  drawerTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#3b5a9a',
+    backgroundColor: '#ffeedd',
+    padding: 10,
+    borderRadius: 10,
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    marginLeft: 10,
+  },
+  drawerItemIcon: {
+    marginRight: 10,
+  },
+  drawerItemText: {
+    fontSize: 18,
+    color: '#567100',
+  },
 
-    // buttonsContainer: {
-    //     flex: 1,
-    //     flexDirection: 'column',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     marginTop: 50,
-    //     width: '90%',
-    //     // gap: 10
-    // },
-    button: {
-        backgroundColor: '#3b9a67', // '#007AFF',
-        padding: 15,
-        // flex: 1,
-        // height: 50,
-        // flexDirection: 'column',
-        borderRadius: 50,
-        marginBottom: 15,
-        marginRight: 220,
-        width: '40%',
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-        fontWeight: 'bold',
-        // textAlign: 'center',
-    },
-    // zebra
-    zebraImage: {
-        width: 280,
-        height: 380,
-        resizeMode: 'contain',
-        position: 'absolute',
-        marginLeft: 10,
-        right: -30
-    },
+  // Advertisement styles
+  adContainer: {
+    width: '100%',
+    paddingVertical: 10,
+    backgroundColor: '#f2f2f2',
+    borderTopWidth: 1,
+    borderColor: '#ddd',
+    alignItems: 'center',
+  },
+  adText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#de0000',
+    marginBottom: 5,
+  },
+  adImage: {
+    width: 300,
+    height: 45,
+  },
+});
 
-    // Drawer styles
-    drawerContainer: {
-        flex: 1,
-        backgroundColor: '#fff',
-        padding: 20,
-    },
-    drawerTitle: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        marginBottom: 20,
-        color: '#3b5a9a',
-        backgroundColor: '#ffeedd',
-        // textAlign: 'center',
-        padding: 10,
-        borderRadius: 10,
-
-    },
-    drawerItem: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingVertical: 15,
-        marginLeft: 10
-    },
-    drawerItemIcon: {
-        marginRight: 10,
-    },
-    drawerItemText: {
-        fontSize: 18,
-        color: '#567100',
-    },
-
-    // Advertisement styles
-    adContainer: {
-        width: '100%',
-        paddingVertical: 10,
-        backgroundColor: '#f2f2f2',
-        borderTopWidth: 1,
-        borderColor: '#ddd',
-        alignItems: 'center',
-    },
-    adText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#de0000',
-        marginBottom: 5,
-    },
-    adImage: {
-        width: 300,
-        height: 45,
-    },
-
-})
-
-export default HomeScreen
+export default HomeScreen;
